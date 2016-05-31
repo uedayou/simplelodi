ad4f5115264d7a1ba1256d4b3b6da12044d37834
uedayou <yooueda@gmail.com>
2016-05-31 09:54:20 +0900

    ディレクトリ構成変更

--- simplelodi.php
+++ simplelodi.php
@@ -7,74 +7,14 @@
  *
  */
 
-include 'Negotiation/AbstractNegotiator.php';
-include 'Negotiation/Match.php';
-include 'Negotiation/BaseAccept.php';
-include 'Negotiation/Exception/Exception.php';
-include 'Negotiation/Exception/InvalidArgument.php';
-include 'Negotiation/Exception/InvalidMediaType.php';
-include 'Negotiation/AcceptHeader.php';
-include 'Negotiation/Accept.php';
-include 'Negotiation/Negotiator.php';
-
-include 'IRI/IRI.php';
-include 'JsonLD/GraphInterface.php';
-include 'JsonLD/JsonLdSerializable.php';
-include 'JsonLD/Value.php';
-include 'JsonLD/LanguageTaggedString.php';
-include 'JsonLD/TypedValue.php';
-include 'JsonLD/RdfConstants.php';
-include 'JsonLD/NodeInterface.php';
-include 'JsonLD/Node.php';
-include 'JsonLD/Graph.php';
-include 'JsonLD/JsonLD.php';
-
-include 'easyrdf/lib/Http/Client.php';
-include 'easyrdf/lib/Http/Response.php';
-include 'easyrdf/lib/Http.php';
-include 'easyrdf/lib/RdfNamespace.php';
-include 'easyrdf/lib/Literal.php';
-include 'easyrdf/lib/Resource.php';
-include 'easyrdf/lib/Parser.php';
-include 'easyrdf/lib/Serialiser.php';
-include 'easyrdf/lib/Format.php';
-include 'easyrdf/lib/Utils.php';
-include 'easyrdf/lib/TypeMapper.php';
-include 'easyrdf/lib/Exception.php';
-include 'easyrdf/lib/Parser/RdfPhp.php';
-include 'easyrdf/lib/Parser/Exception.php';
-include 'easyrdf/lib/Parser/Json.php';
-include 'easyrdf/lib/Parser/JsonLd.php';
-include 'easyrdf/lib/Parser/Ntriples.php';
-include 'easyrdf/lib/Parser/Rapper.php';
-include 'easyrdf/lib/Parser/Rdfa.php';
-include 'easyrdf/lib/Parser/Arc.php';
-include 'easyrdf/lib/Parser/RdfXml.php';
-include 'easyrdf/lib/Parser/Redland.php';
-include 'easyrdf/lib/Parser/Turtle.php';
-include 'easyrdf/lib/ParsedUri.php';
-include 'easyrdf/lib/Serialiser/RdfPhp.php';
-include 'easyrdf/lib/Serialiser/GraphViz.php';
-include 'easyrdf/lib/Serialiser/Json.php';
-include 'easyrdf/lib/Serialiser/JsonLd.php';
-include 'easyrdf/lib/Serialiser/Ntriples.php';
-include 'easyrdf/lib/Serialiser/Rapper.php';
-include 'easyrdf/lib/Serialiser/Arc.php';
-include 'easyrdf/lib/Serialiser/RdfXml.php';
-include 'easyrdf/lib/Serialiser/Turtle.php';
-include 'easyrdf/lib/Literal/HexBinary.php';
-include 'easyrdf/lib/Literal/Boolean.php';
-include 'easyrdf/lib/Literal/Date.php';
-include 'easyrdf/lib/Literal/DateTime.php';
-include 'easyrdf/lib/Literal/Decimal.php';
-include 'easyrdf/lib/Literal/HTML.php';
-include 'easyrdf/lib/Literal/Integer.php';
-include 'easyrdf/lib/Literal/XML.php';
-include 'easyrdf/lib/Graph.php';
+require_once 'simplelodi_load.php';
 
 define("DATA_DIR","data/");
 
-define("DEBUG",false);
+define("DATA_TYPE", "turtle");
+define("DATA_EXTENSION", ".ttl");
+
+define("DEBUG",true);
 
 // Warning 表示しない
 error_reporting(0);
@@ -119,7 +59,7 @@ if ($mediaType!=null) {
 	$value = 'text/html';
 }
 
-$path = DATA_DIR.$path.".ttl";
+$path = DATA_DIR.$path.DATA_EXTENSION;
 
 if (!file_exists($path)) {
 	// ファイルがないとき
@@ -131,7 +71,7 @@ if (!file_exists($path)) {
 $graph = new \EasyRdf\Graph();
 //$rdf->load();
 //$graph->parse($data, "turtle");
-$graph->parseFile($path, "turtle");
+$graph->parseFile($path, DATA_TYPE);
 
 $format = "turtle";// "php"; // html
 if (preg_match("/xml/i", $value)) {
---
+++ simplelodi_load.php
@@ -0,0 +1,68 @@
+<?php
+
+include 'lib/Negotiation/AbstractNegotiator.php';
+include 'lib/Negotiation/Match.php';
+include 'lib/Negotiation/BaseAccept.php';
+include 'lib/Negotiation/Exception/Exception.php';
+include 'lib/Negotiation/Exception/InvalidArgument.php';
+include 'lib/Negotiation/Exception/InvalidMediaType.php';
+include 'lib/Negotiation/AcceptHeader.php';
+include 'lib/Negotiation/Accept.php';
+include 'lib/Negotiation/Negotiator.php';
+
+include 'lib/IRI/IRI.php';
+include 'lib/JsonLD/GraphInterface.php';
+include 'lib/JsonLD/JsonLdSerializable.php';
+include 'lib/JsonLD/Value.php';
+include 'lib/JsonLD/LanguageTaggedString.php';
+include 'lib/JsonLD/TypedValue.php';
+include 'lib/JsonLD/RdfConstants.php';
+include 'lib/JsonLD/NodeInterface.php';
+include 'lib/JsonLD/Node.php';
+include 'lib/JsonLD/Graph.php';
+include 'lib/JsonLD/JsonLD.php';
+
+include 'lib/easyrdf/lib/Http/Client.php';
+include 'lib/easyrdf/lib/Http/Response.php';
+include 'lib/easyrdf/lib/Http.php';
+include 'lib/easyrdf/lib/RdfNamespace.php';
+include 'lib/easyrdf/lib/Literal.php';
+include 'lib/easyrdf/lib/Resource.php';
+include 'lib/easyrdf/lib/Parser.php';
+include 'lib/easyrdf/lib/Serialiser.php';
+include 'lib/easyrdf/lib/Format.php';
+include 'lib/easyrdf/lib/Utils.php';
+include 'lib/easyrdf/lib/TypeMapper.php';
+include 'lib/easyrdf/lib/Exception.php';
+include 'lib/easyrdf/lib/Parser/RdfPhp.php';
+include 'lib/easyrdf/lib/Parser/Exception.php';
+include 'lib/easyrdf/lib/Parser/Json.php';
+include 'lib/easyrdf/lib/Parser/JsonLd.php';
+include 'lib/easyrdf/lib/Parser/Ntriples.php';
+include 'lib/easyrdf/lib/Parser/Rapper.php';
+include 'lib/easyrdf/lib/Parser/Rdfa.php';
+include 'lib/easyrdf/lib/Parser/Arc.php';
+include 'lib/easyrdf/lib/Parser/RdfXml.php';
+include 'lib/easyrdf/lib/Parser/Redland.php';
+include 'lib/easyrdf/lib/Parser/Turtle.php';
+include 'lib/easyrdf/lib/ParsedUri.php';
+include 'lib/easyrdf/lib/Serialiser/RdfPhp.php';
+include 'lib/easyrdf/lib/Serialiser/GraphViz.php';
+include 'lib/easyrdf/lib/Serialiser/Json.php';
+include 'lib/easyrdf/lib/Serialiser/JsonLd.php';
+include 'lib/easyrdf/lib/Serialiser/Ntriples.php';
+include 'lib/easyrdf/lib/Serialiser/Rapper.php';
+include 'lib/easyrdf/lib/Serialiser/Arc.php';
+include 'lib/easyrdf/lib/Serialiser/RdfXml.php';
+include 'lib/easyrdf/lib/Serialiser/Turtle.php';
+include 'lib/easyrdf/lib/Literal/HexBinary.php';
+include 'lib/easyrdf/lib/Literal/Boolean.php';
+include 'lib/easyrdf/lib/Literal/Date.php';
+include 'lib/easyrdf/lib/Literal/DateTime.php';
+include 'lib/easyrdf/lib/Literal/Decimal.php';
+include 'lib/easyrdf/lib/Literal/HTML.php';
+include 'lib/easyrdf/lib/Literal/Integer.php';
+include 'lib/easyrdf/lib/Literal/XML.php';
+include 'lib/easyrdf/lib/Graph.php';
+
+?>
