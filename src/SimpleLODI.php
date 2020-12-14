9a85340b11773178f6112945f87e591dc42e8451
uedayou <yooueda@gmail.com>
2020-12-14 23:58:34 +0900

    微修正

--- src/SimpleLODI.php
+++ src/SimpleLODI.php
@@ -147,7 +147,7 @@ class SimpleLODI {
 	}
 
 	protected function unicode_decode($str) {
-		return preg_replace_callback('/\\\\\\\\u([0-9a-f]{4})/i', function($match) {
+		return preg_replace_callback('/\\\\\\\\u([0-9a-fA-F]{4})/i', function($match) {
 			return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
 		}, $str);
 	}
